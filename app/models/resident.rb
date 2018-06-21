2995e17c3fe94e458e17cdb37ba6c9567d5dcd84
abourke09 <aabourke@gmail.com>
2018-06-21 11:59:10 -0700

    remove last references to apartment_building and update config file

--- app/models/resident.rb
+++ app/models/resident.rb
@@ -1,5 +1,5 @@
 class Resident < ActiveRecord::Base
   has_secure_password
   has_many :service_requests
-  belongs_to :apartment_building
+  belongs_to :building
 end
--- app/models/service_request.rb
+++ app/models/service_request.rb
@@ -1,4 +1,4 @@
 class ServiceRequest < ActiveRecord::Base
   belongs_to :resident
-  belongs_to :apartment_building, through: resident
+  belongs_to :building, through: resident
 end
