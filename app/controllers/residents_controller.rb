467593f5a8c19f218f11726f727955013b43a7ce
abourke09 <aabourke@gmail.com>
2018-06-26 12:30:05 -0700

    adds validation on models, debugs controller code

--- app/controllers/residents_controller.rb
+++ app/controllers/residents_controller.rb
@@ -10,17 +10,6 @@ class ResidentsController < ApplicationController
     end
   end
 
-  #Residents' homepage, show page
-  get "/residents/:slug" do
-    if logged_in?
-      binding.pry
-      @resident = current_user
-      erb :"/residents/show"
-    else
-      redirect to "/"
-    end
-  end
-
   #Signup page- POST action
   post "/residents" do
     if params[:username] == "" || params[:password] == "" || params[:name] == "" || params[:building] == "" || params[:apartment_number] == ""
@@ -35,7 +24,7 @@ class ResidentsController < ApplicationController
       )
       @resident.save
       session[:user_id] = @resident.id
-ex      redirect to "/residents/#{@resident.slug}"
+      redirect to "/residents/#{@resident.slug}"
     end
   end
 
@@ -49,9 +38,9 @@ ex      redirect to "/residents/#{@resident.slug}"
       end
     end
 
-  #Login Page- Form Submit
+  #Login Page- POST action
     post '/login' do
-      resident = Resident.find_by(username: params[:username])
+      @resident = Resident.find_by(username: params[:username])
 
       if resident && resident.authenticate(params[:password])
         session[:user_id] = resident.id
@@ -61,4 +50,14 @@ ex      redirect to "/residents/#{@resident.slug}"
       end
     end
 
+
+    #Residents' homepage, show page
+    get "/residents/:slug" do
+      @resident = Resident.find_by_slug(params[:slug])
+      if logged_in?
+        erb :"/residents/show"
+      else
+        redirect to "/"
+      end
+    end
 end
--- app/models/resident.rb
+++ app/models/resident.rb
@@ -2,6 +2,12 @@ class Resident < ActiveRecord::Base
   has_secure_password
   has_many :service_requests
   belongs_to :building
+  validates :username,  uniqueness: true
+  validates :username,  presence: true
+  validates :name,  presence: true
+  validates :building_id,  presence: true
+  validates :apt_number,  presence: true
+
 
   def slug
     username.downcase.gsub(" ","-")
