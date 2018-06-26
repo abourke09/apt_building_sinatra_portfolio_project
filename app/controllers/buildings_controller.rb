f7a7755b09310307f52338a2785115500479bab4
abourke09 <aabourke@gmail.com>
2018-06-26 16:36:05 -0700

    creates list of all buildings and show page for each with residents usernames listed

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -2,36 +2,17 @@ class BuildingsController < ApplicationController
 
   # GET: /buildings
   get "/buildings" do
-    erb :"/buildings/index.html"
-  end
-
-  # GET: /buildings/new
-  get "/buildings/new" do
-    erb :"/buildings/new.html"
-  end
-
-  # POST: /buildings
-  post "/buildings" do
-    redirect "/buildings"
+    erb :"/buildings/index"
   end
 
   # GET: /buildings/5
   get "/buildings/:id" do
-    erb :"/buildings/show.html"
+    if logged_in?
+      @building = Building.find_by(params[:id])
+      erb :"/buildings/show"
+    else
+      redirect to "/login"
+    end
   end
 
-  # GET: /buildings/5/edit
-  get "/buildings/:id/edit" do
-    erb :"/buildings/edit.html"
-  end
-
-  # PATCH: /buildings/5
-  patch "/buildings/:id" do
-    redirect "/buildings/:id"
-  end
-
-  # DELETE: /buildings/5/delete
-  delete "/buildings/:id/delete" do
-    redirect "/buildings"
-  end
 end
