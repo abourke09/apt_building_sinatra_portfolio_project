65a9986d20cf3753103f73160db88bf904ea6bdb
abourke09 <aabourke@gmail.com>
2018-06-27 10:38:01 -0700

    adds seeds for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,113 @@ Building.create(name: "Van Ness")
 Building.create(name: "MB360")
 Building.create(name: "Avalon Hayes Valley")
 
-@example_resident = Resident.create(
+Resident.create(
+  :username => "joycemom808",
+  :password => "waffles",
+  :name => "Joyce Schwen",
+  :apt_number => 22,
+  :building_id => 1,
+)
+
+Resident.create(
+  :username => "oceanbob74",
+  :password => "pancakes",
+  :name => "Bob Bourke",
+  :apt_number => 20,
+  :building_id => 2,
+)
+
+Resident.create(
+  :username => "jeffyboy",
+  :password => "bagels",
+  :name => "Jeff Bourke",
+  :apt_number => 4,
+  :building_id => 2,
+)
+
+Resident.create(
+  :username => "katesmgates",
+  :password => "muffins",
+  :name => "Katie Bourke",
+  :apt_number => 8,
+  :building_id => 3,
+)
+
+Resident.create(
+  :username => "lauralou22",
+  :password => "crepes",
+  :name => "Laura Jude",
+  :apt_number => 9,
+  :building_id => 4,
+)
+
+Resident.create(
+  :username => "dylanomilano",
+  :password => "toast",
+  :name => "Dylan Jude",
+  :apt_number => 11,
+  :building_id => 5,
+)
+
+Resident.create(
+  :username => "mcTT",
+  :password => "cake",
+  :name => "Marie Christine Jude",
+  :apt_number => 5,
+  :building_id => 6,
+)
+
+Resident.create(
   :username => "abourke09",
-  :password => "pancakes!",
+  :password => "pancakes",
   :name => "Annie Bourke",
   :apt_number => 3,
   :building_id => 7,
+)
+
+Resident.create(
+  :username => "caryshark",
+  :password => "bananabread",
+  :name => "Cary Chiswick",
+  :apt_number => 7,
+  :building_id => 7,
+)
+
+Resident.create(
+  :username => "ranigirl",
+  :password => "scones",
+  :name => "Ranitri",
+  :apt_number => 14,
+  :building_id => 7,
+)
+Resident.create(
+  :username => "rachyrach",
+  :password => "blintz",
+  :name => "Rachael Jude",
+  :apt_number => 99,
+  :building_id => 8,
+)
+
+Resident.create(
+  :username => "blakedude",
+  :password => "omlette",
+  :name => "Blake Davis",
+  :apt_number => 22,
+  :building_id => 9,
+)
+
+Resident.create(
+  :username => "coolgirlmik",
+  :password => "avocadotoast",
+  :name => "Mikayla Markrich",
+  :apt_number => 1,
+  :building_id => 10,
+)
+
+Resident.create(
+  :username => "bigmax",
+  :password => "oatmeal",
+  :name => "Max Markrich",
+  :apt_number => 1,
+  :building_id => 10,
 )
